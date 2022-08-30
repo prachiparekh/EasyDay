9a191673908a178313ab8452cfd79dcca1719233
prachiparekh711 <prachi.cubezytech@gmail.com>
2022-08-30 15:05:39 +0530

    Commit 30/08/2022
    Auth screen UI

--- app/src/main/java/com/civil/easyday/screens/activities/boarding/OnBoardingActivity.kt
+++ app/src/main/java/com/civil/easyday/screens/activities/boarding/OnBoardingActivity.kt
@@ -3,18 +3,34 @@ package com.civil.easyday.screens.activities.boarding
 import com.civil.easyday.R
 import com.civil.easyday.screens.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_on_boarding.*
+import ro.westaco.carhome.presentation.screens.onboarding.OnboardingItem
 
 @AndroidEntryPoint
-class OnBoardingActivity : BaseActivity<OnBoardingViewModel>() {
-
-
-    override fun getContentView()=R.layout.activity_on_boarding
+class OnBoardingActivity : BaseActivity<OnBoardingViewModel>(),
+    OnBoardingAdapter.BoardingInterface {
+    private lateinit var adapter: OnBoardingAdapter
+    var items: List<OnboardingItem> = arrayListOf()
+    override fun getContentView() = R.layout.activity_on_boarding
 
     override fun setupUi() {
+        adapter = OnBoardingAdapter()
+        pager.adapter = adapter
 
     }
 
     override fun setupObservers() {
+        viewModel.pagerItems.observe(this) { items ->
+            this.items = items
+            adapter.setData(items, this)
+        }
+    }
 
+    override fun onClickNext(nextPosition: Int) {
+
+        if (nextPosition == items.size)
+            viewModel.navigateToAuth()
+        else
+            pager.currentItem = nextPosition
     }
 }
---
+++ app/src/main/java/com/civil/easyday/screens/activities/boarding/OnBoardingAdapter.kt
@@ -0,0 +1,82 @@
+package com.civil.easyday.screens.activities.boarding
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.civil.easyday.R
+import ro.westaco.carhome.presentation.screens.onboarding.OnboardingItem
+
+class OnBoardingAdapter(
+    private var items: List<OnboardingItem> = arrayListOf()
+
+) : RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder>() {
+
+    var anInterface: BoardingInterface? = null
+
+    interface BoardingInterface {
+        fun onClickNext(nextPosition: Int)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = OnBoardingViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_onboarding_pager, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: OnBoardingViewHolder, position: Int) {
+        val item = items[position]
+        val context = holder.itemView.context
+
+        holder.title.text = context.getString(item.titleResId)
+        holder.image.setImageDrawable(ContextCompat.getDrawable(context, item.imageResId))
+
+        if (position == items.size - 1)
+            holder.cta.text = context.getString(R.string.get_started)
+        else
+            holder.cta.text = context.getString(R.string.next)
+
+        when (position) {
+            0 -> {
+                holder.image1.setImageDrawable(context.resources.getDrawable(R.drawable.ic_boarding_dash))
+                holder.image2.setImageDrawable(context.resources.getDrawable(R.drawable.ic_bording_dot))
+                holder.image3.setImageDrawable(context.resources.getDrawable(R.drawable.ic_bording_dot))
+            }
+            1 -> {
+                holder.image1.setImageDrawable(context.resources.getDrawable(R.drawable.ic_bording_dot))
+                holder.image2.setImageDrawable(context.resources.getDrawable(R.drawable.ic_boarding_dash))
+                holder.image3.setImageDrawable(context.resources.getDrawable(R.drawable.ic_bording_dot))
+            }
+            2 -> {
+                holder.image1.setImageDrawable(context.resources.getDrawable(R.drawable.ic_bording_dot))
+                holder.image2.setImageDrawable(context.resources.getDrawable(R.drawable.ic_bording_dot))
+                holder.image3.setImageDrawable(context.resources.getDrawable(R.drawable.ic_boarding_dash))
+            }
+        }
+
+        holder.cta.setOnClickListener {
+            anInterface?.onClickNext(position + 1)
+        }
+
+    }
+
+    override fun getItemCount() = items.size
+
+    class OnBoardingViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView = view.findViewById(R.id.title)
+        val cta: TextView = view.findViewById(R.id.cta)
+        val image: ImageView = view.findViewById(R.id.img)
+        val image1: ImageView = view.findViewById(R.id.img1)
+        val image2: ImageView = view.findViewById(R.id.img2)
+        val image3: ImageView = view.findViewById(R.id.img3)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(items: List<OnboardingItem>, anInterface: BoardingInterface) {
+        this.items = items
+        this.anInterface = anInterface
+        notifyDataSetChanged()
+    }
+}
