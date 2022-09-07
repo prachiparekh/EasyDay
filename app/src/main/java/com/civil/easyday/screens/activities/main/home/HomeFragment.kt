c86781b89d8b7c205fcf49f05871af535501e5ce
prachiparekh711 <prachi.cubezytech@gmail.com>
2022-09-07 17:40:02 +0530

    Commit 07092022
    ShowcaseView UI

--- app/src/main/java/com/civil/easyday/screens/activities/main/home/HomeFragment.kt
+++ app/src/main/java/com/civil/easyday/screens/activities/main/home/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.civil.easyday.screens.activities.main.home
 
 
+import android.os.Handler
 import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.TextView
 import com.civil.easyday.R
 import com.civil.easyday.screens.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import me.toptas.fancyshowcase.FancyShowCaseQueue
 import me.toptas.fancyshowcase.FancyShowCaseView
 import me.toptas.fancyshowcase.listener.OnViewInflateListener
 
@@ -18,49 +21,74 @@ class HomeFragment : BaseFragment<HomeViewModel>() {
     }
 
     var showCaseDone = false
+    private lateinit var queue: FancyShowCaseQueue
+    private lateinit var fancyView2: FancyShowCaseView
+    private lateinit var fancyView1: FancyShowCaseView
 
     override fun getContentView() = R.layout.fragment_home
 
     override fun initUi() {
 
-        var showcaseView2: FancyShowCaseView? = null
-        var showcaseView1: FancyShowCaseView? = null
+
         if (!showCaseDone) {
-            showcaseView2 = FancyShowCaseView.Builder(requireActivity())
+
+            fancyView2 = FancyShowCaseView.Builder(requireActivity())
                 .customView(R.layout.showcaseview2, object : OnViewInflateListener {
                     override fun onViewInflated(view: View) {
-                        view.findViewById<View>(R.id.ctaNext).setOnClickListener {
-                            showcaseView2?.hide()
-
-                            showcaseView1 = FancyShowCaseView.Builder(requireActivity())
-                                .customView(R.layout.showcaseview1, object : OnViewInflateListener {
-                                    override fun onViewInflated(view: View) {
-                                        view.findViewById<View>(R.id.done).setOnClickListener {
-                                            showCaseDone = true
-                                            showcaseView1?.hide()
-                                        }
-                                    }
-                                })
-                                .fitSystemWindows(true)
-                                .build()
-
-                            showcaseView1?.show()
-                        }
-                        view.findViewById<View>(R.id.skip).setOnClickListener {
-                            showCaseDone = true
-                            showcaseView2?.hide()
-                        }
+                        setAnimatedContent(view, fancyView2)
                     }
-
                 })
-                .fitSystemWindows(true)
                 .build()
-            showcaseView2.show()
+
+            fancyView1 = FancyShowCaseView.Builder(requireActivity())
+                .customView(R.layout.showcaseview1, object : OnViewInflateListener {
+                    override fun onViewInflated(view: View) {
+                        setAnimatedContent(view, fancyView1)
+                    }
+                })
+                .build()
+
+            queue = FancyShowCaseQueue().apply {
+                add(fancyView2)
+                add(fancyView1)
+                show()
+            }
         }
 
     }
 
     override fun setObservers() {
+    }
+
+    private fun setAnimatedContent(view: View, fancyShowCaseView: FancyShowCaseView) {
+        Handler().postDelayed({
+
+            val mainAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_in_left)
+            mainAnimation.fillAfter = true
+
+            val subAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_in_left)
+            subAnimation.fillAfter = true
+
+            if (fancyShowCaseView == fancyView2) {
+                val ctaNext = view.findViewById<View>(R.id.ctaNext) as TextView
+                val text2 = view.findViewById<View>(R.id.text2) as TextView
+                val skip = view.findViewById<View>(R.id.skip) as TextView
+                ctaNext.setOnClickListener { fancyShowCaseView.hide() }
+                skip.setOnClickListener {
+                    showCaseDone = true
+                    queue.cancel(true)
+                }
+                text2.startAnimation(mainAnimation)
+            } else {
+                val done = view.findViewById<View>(R.id.done) as TextView
+                val text1 = view.findViewById<View>(R.id.text1) as TextView
+                done.setOnClickListener {
+                    showCaseDone = true
+                    queue.cancel(true)
+                }
+                Handler().postDelayed({ text1.startAnimation(subAnimation) }, 80)
+            }
+        }, 200)
     }
 
 }
